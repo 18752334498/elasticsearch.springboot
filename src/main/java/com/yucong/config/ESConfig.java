9627a1390a868f45246611cc50a536501b3128ed
2424693824ls <2424693824@qq.com>
2019-07-12 00:01:56 +0800

    afdsfsdf

--- src/main/java/com/yucong/config/ESConfig.java
+++ src/main/java/com/yucong/config/ESConfig.java
@@ -3,47 +3,38 @@ package com.yucong.config;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
-@ComponentScan(basePackageClasses = ESClientSpringFactory.class)
 public class ESConfig {
-	@Value("${elasticSearch.host}")
-	private String host;
 
-	@Value("${elasticSearch.port}")
-	private int port;
+    private String host = "127.0.0.1";
+    private int port = 9200;
+    private Integer connectNum = 10;
+    private Integer connectPerRoute = 50;
 
-	@Value("${elasticSearch.client.connectNum}")
-	private Integer connectNum;
+    @Bean
+    public HttpHost httpHost() {
+        return new HttpHost(host, port, "http");
+    }
 
-	@Value("${elasticSearch.client.connectPerRoute}")
-	private Integer connectPerRoute;
+    @Bean(initMethod = "init", destroyMethod = "close")
+    public ESClientSpringFactory getFactory() {
+        return ESClientSpringFactory.build(httpHost(), connectNum, connectPerRoute);
+    }
 
-	@Bean
-	public HttpHost httpHost() {
-		return new HttpHost(host, port, "http");
-	}
+    @Bean
+    @Scope("singleton")
+    public RestClient getRestClient() {
+        return getFactory().getClient();
+    }
 
-	@Bean(initMethod = "init", destroyMethod = "close")
-	public ESClientSpringFactory getFactory() {
-		return ESClientSpringFactory.build(httpHost(), connectNum, connectPerRoute);
-	}
-
-	@Bean
-	@Scope("singleton")
-	public RestClient getRestClient() {
-		return getFactory().getClient();
-	}
-
-	@Bean
-	@Scope("singleton")
-	public RestHighLevelClient getRHLClient() {
-		return getFactory().getRhlClient();
-	}
+    @Bean
+    @Scope("singleton")
+    public RestHighLevelClient getRHLClient() {
+        return getFactory().getRhlClient();
+    }
 
 }
--- src/test/java/com/yucong/ElasticSerachTest.java
+++ src/test/java/com/yucong/ElasticSerachTest.java
@@ -1,11 +1,25 @@
 package com.yucong;
 
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = ElasticSerachApp.class)
 public class ElasticSerachTest {
+
+    @Autowired
+    private RestClient restClient;
+    @Autowired
+    private RestHighLevelClient restHighLevelClient;
+
+    @Test
+    public void test1() {
+
+    }
 
 }
